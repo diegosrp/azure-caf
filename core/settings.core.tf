c6de0a67aebe226b04740f95f3beb9ba6e827bf0
Diego Pauletto <diego.pauletto@spark.co.nz>
2024-12-25 22:14:21 +1300

    Adding subscription_id_overrides

--- core/settings.core.tf
+++ core/settings.core.tf
@@ -4,16 +4,16 @@ locals {
   #---------------------------------------------------------------------------------------------------
   caf_configure_mg_subscriptions = {
     root = [
-      "6e5421ea-88a7-42bc-98cb-84e4844ba8ab", # Personal Subscription 
+
     ],
     decommissioned = [
 
     ],
     landing-zones = [
-      "2a714520-279c-4893-9925-b2a2da5f57ae", # Visual Studio Enterprise Subscription
+      "<SUBSCRIPTION_ID>",
     ],
     platform = [
-
+      "<SUBSCRIPTION_ID>",
     ],
     connectivity = [
 
@@ -27,10 +27,5 @@ locals {
     sandboxes = [
 
     ],
-
   }
-
-
-
-
 }
